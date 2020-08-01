942d743bdfd3b7e8b6b24bb2b0b32c8918a8f100
lezan <zanchileonardo@gmail.com>
2020-08-01 17:20:44 +0200

    Add saveJson, readJson, saveCsv and readCsv.

---
+++ workWithData.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const neatCsv = require('neat-csv');
+const ObjectsToCsv = require('objects-to-csv');
+
+module.exports = {
+	saveJson: (json, filename) => {
+		fs.writeFileSync(`./data/${filename}.json`, JSON.stringify(json));
+	},
+
+	readJson: (filename) => {
+		const importData = fs.readFileSync(filename);
+		const data = JSON.parse(importData);
+
+		return data;
+	},
+
+	saveCsv: async (data, filename) => {
+		const csvData = new ObjectsToCsv(data);
+	 	await csvData.toDisk(`./data/${filename}.csv`);
+	},
+
+	readCsv: async (filename) => {
+		const importData = fs.readFileSync(`./data/${filename}`);
+		const data = await neatCsv(importData, { separator: ',' });
+
+		return data;
+	},
+};
