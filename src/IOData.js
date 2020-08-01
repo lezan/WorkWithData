37859739333bc094c17364af6f6abf7294f7826f
lezan <zanchileonardo@gmail.com>
2020-08-01 18:06:06 +0200

    Fix path.

--- src/IOData.js
+++ src/IOData.js
@@ -4,11 +4,11 @@ const ObjectsToCsv = require('objects-to-csv');
 
 module.exports = {
 	saveJson: (json, filename) => {
-		fs.writeFileSync(`../data/${filename}.json`, JSON.stringify(json));
+		fs.writeFileSync(`./data/${filename}.json`, JSON.stringify(json));
 	},
 
 	readJson: (filename) => {
-		const importData = fs.readFileSync(`../data/${filename}.json`);
+		const importData = fs.readFileSync(`./data/${filename}.json`);
 		const data = JSON.parse(importData);
 
 		return data;
@@ -16,11 +16,11 @@ module.exports = {
 
 	saveCsv: async (data, filename) => {
 		const csvData = new ObjectsToCsv(data);
-	 	await csvData.toDisk(`../data/${filename}.csv`);
+	 	await csvData.toDisk(`./data/${filename}.csv`);
 	},
 
 	readCsv: async (filename) => {
-		const importData = fs.readFileSync(`../data/${filename}.csv`);
+		const importData = fs.readFileSync(`./data/${filename}.csv`);
 		const data = await neatCsv(importData, { separator: ',' });
 
 		return data;
