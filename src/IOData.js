6899757b538648be776752f6d63354b0fe0d9f09
lezan <zanchileonardo@gmail.com>
2020-08-10 15:34:19 +0200

    New function to save csv -> saveCsv2.

--- src/IOData.js
+++ src/IOData.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const neatCsv = require('neat-csv');
 const ObjectsToCsv = require('objects-to-csv');
+const fastCsv = require('fast-csv');
 
 module.exports = {
 	saveJson: (json, filename) => {
@@ -18,6 +19,19 @@ module.exports = {
 		const csvData = new ObjectsToCsv(data);
 	 	await csvData.toDisk(`./data/${filename}.csv`);
 	},
+
+	/*
+	A volte la precedente funzione saveCsv mi ha dato problemi, quindi ho introdotto una seconda funzione
+	per scrivere i file csv. Nei casi in cui la prima funzionava, questa seconda non dava problemi.
+	Le lascio entrambe.
+	*/
+	saveCsv2: (data, filename) => {
+		const ws = fs.createWriteStream(`./data/${filename}.csv`);
+
+		fastCsv.write(data, { headers: true })
+			.pipe(ws);
+	},
+
 
 	readCsv: async (filename) => {
 		const importData = fs.readFileSync(`./data/${filename}.csv`);
